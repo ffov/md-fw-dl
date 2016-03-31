104088ff05dd82c58f87d97fce9a434dd6255824
Simon Wüllhorst <mail@simon-wuellhorst.de>
2016-03-31 22:25:32 +0200

    Added domaene08 (Gescher), domaene12 (Duelmen) and domaene13 (Rorup). Changed shapefiles of domaene02 (Kreis Coesfeld) and domaene06 (WML).

--- config.js
+++ config.js
@@ -14,8 +14,11 @@ sites:{
 "domaene04" : {id: "domaene04",short:"d04",name:"Kreis Steinfurt Ost", version:"1.0.4", version_base:"2016.1.2"},
 "domaene05" : {id: "domaene05",short:"d05",name:"Münster Süd", version:"1.0.4", version_base:"2016.1.2"},
 "domaene06" : {id: "domaene06",short:"d06",name:"Westmünsterland", version:"1.0.4", version_base:"2016.1.2"},
+"domaene08" : {id: "domaene08",short:"d06",name:"Gescher", version:"1.0.4", version_base:"2016.1.2"},
 "domaene09" : {id: "domaene09",short:"d09",name:"Stadtlohn", version:"1.0.5", version_base:"2016.1.x"},
 "domaene11" : {id: "domaene11",short:"d11",name:"Bocholt", version:"1.0.5", version_base:"2016.1.x"},
+"domaene12" : {id: "domaene12",short:"d02",name:"Dülmen (ohne Rorup)", version:"1.0.4", version_base:"2016.1.2"},
+"domaene13" : {id: "domaene13",short:"d02",name:"Rorup", version:"1.0.4", version_base:"2016.1.2"},
 "domaene14" : {id: "domaene14",short:"d14",name:"Kreis Warendorf", version:"1", version_base:"2016.1.1"},
 },
 

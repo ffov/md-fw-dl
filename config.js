fdd4a273a9a9bf5767f0d2761614b793016e41c8
Simon Wüllhorst <mail@simon-wuellhorst.de>
2016-05-27 11:59:18 +0200

    - Polygon für Selm als Domäne 27 umbenannt  - Firmware für die neuen Domänen 17-27 (außer 26) aktiviert  - Version für Domäne 16 auf 1.1.4 erhöht

--- config.js
+++ config.js
@@ -21,17 +21,17 @@ sites:{
   "domaene12" : {id: "domaene12",short:"d12",name:"Dülmen (ohne Rorup)", version:"1.0.9", version_base:"2016.1.3", on_map: true },
   "domaene13" : {id: "domaene13",short:"d13",name:"Rorup", version:"1.0.9", version_base:"2016.1.3", on_map: true },
   "domaene14" : {id: "domaene14",short:"d14",name:"Kreis Warendorf", version:"1", version_base:"2016.1.1", on_map: true },
-  "domaene16" : {id: "domaene16",short:"d16",name:"Münster Promenade", version:"1.0.8", version_base:"2016.1.3", on_map: true },
-  "domaene17" : {id: "domaene17",short:"d17",name:"Emsdetten", version:"1.0.4", version_base:"2016.1.2", proxy_to: "domaene04", on_map: true },
-  "domaene18" : {id: "domaene18",short:"d18",name:"Greven", version:"1.0.4", version_base:"2016.1.2", proxy_to: "domaene04", on_map: true },
-  "domaene19" : {id: "domaene19",short:"d19",name:"Neuenkirchen", version:"1.0.4", version_base:"2016.1.2", proxy_to: "domaene04", on_map: true },
-  "domaene20" : {id: "domaene20",short:"d20",name:"Ochtrup", version:"1.0.4", version_base:"2016.1.2", proxy_to: "domaene03", on_map: true },
-  "domaene21" : {id: "domaene21",short:"d21",name:"Rheine", version:"1.0.4", version_base:"2016.1.2", proxy_to: "domaene04", on_map: true },
-  "domaene22" : {id: "domaene22",short:"d22",name:"Steinfurt", version:"1.0.4", version_base:"2016.1.2", proxy_to: "domaene03", on_map: true },
-  "domaene23" : {id: "domaene23",short:"d23",name:"Steinfurt-West", version:"1.0.4", version_base:"2016.1.2", proxy_to: "domaene03", on_map: true },
-  "domaene24" : {id: "domaene24",short:"d24",name:"Wettringen", version:"1.0.4", version_base:"2016.1.2", proxy_to: "domaene03", on_map: true },
-  "domaene25" : {id: "domaene25",short:"d25",name:"Steinfurt-Ost", version:"1.0.4", version_base:"2016.1.2", proxy_to: "domaene04", on_map: true },
-  "domaene101" : {id: "domaene101",short:"d101",name:"Selm", proxy_to: "domaene02", on_map: true }
+  "domaene16" : {id: "domaene16",short:"d16",name:"Münster Promenade", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene17" : {id: "domaene17",short:"d17",name:"Emsdetten", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene18" : {id: "domaene18",short:"d18",name:"Greven", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene19" : {id: "domaene19",short:"d19",name:"Neuenkirchen", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene20" : {id: "domaene20",short:"d20",name:"Ochtrup", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene21" : {id: "domaene21",short:"d21",name:"Rheine", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene22" : {id: "domaene22",short:"d22",name:"Steinfurt", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene23" : {id: "domaene23",short:"d23",name:"Steinfurt-West", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene24" : {id: "domaene24",short:"d24",name:"Wettringen", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene25" : {id: "domaene25",short:"d25",name:"Steinfurt-Ost", version:"1.1.4", version_base:"2016.1.5", on_map: true },
+  "domaene27" : {id: "domaene27",short:"d27",name:"Selm", version:"1.1.3", version_base:"2016.1.x", on_map: true }
 },
 
 //router list for gluon v2015.1
